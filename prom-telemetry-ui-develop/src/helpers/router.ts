6aa22511262bc9197b659c4fa56921bbe906589c
Giannis-Man-Prom <g.manousakis.promracing@gmail.com>
2025-04-14 21:15:19 +0300

    Update router.ts

--- prom-telemetry-ui-develop/src/helpers/router.ts
+++ prom-telemetry-ui-develop/src/helpers/router.ts
@@ -1,5 +1,7 @@
+/* Εδώ καθορίζουμε τα paths του router */
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router"
 
+/* Εδώ κάνουμε import τα views μας */
 import Dashboard from "../views/Dashboard_vd.vue"
 import Dash_LiveTelemetry from "../views/Dash_LiveTelemetry.vue"
 
@@ -7,12 +9,12 @@ import ShowCustomCharts from "../views/ShowCustomCharts.vue";
 import csvreceiver from "../components/csvreceiver.vue";
 
 
-
+/* Εδώ έχουμε την αντιστοιχία των paths του site και των views */
 const routes: Array<RouteRecordRaw> = [
 	{
 		path: "/vd/telemetry",
 		name: "Dashboard",
-		component: Dashboard,
+		component: Dashboard, /* Το component που φορτώνεται όταν είμαστε στο παραπάνω link */
 	},
 		{
 		path: "/",
@@ -31,9 +33,10 @@ const routes: Array<RouteRecordRaw> = [
 	}
 ]
 
+/* Εδώ δημιουργούμε router */
 const router = createRouter({
-	history: createWebHistory(),
-	routes,
+	history: createWebHistory(), /* Έτσι μπορούμε να κάνουμε navigate χωρίς να κάνουμε reload τη σελίδα */
+	routes, /* Με τα παραπάνω routes που ορίσαμε */
 })
 
 export default router
