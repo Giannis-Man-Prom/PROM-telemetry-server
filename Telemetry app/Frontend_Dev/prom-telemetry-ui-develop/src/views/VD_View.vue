967cffab080cee48d19dbd290b874e538a9b8044
Giannis-Man-Prom <g.manousakis.promracing@gmail.com>
2025-05-03 15:51:48 +0300

    Change VD View

--- Telemetry app/Frontend_Dev/prom-telemetry-ui-develop/src/views/VD_View.vue
+++ Telemetry app/Frontend_Dev/prom-telemetry-ui-develop/src/views/VD_View.vue
@@ -1,37 +1,76 @@
 <!-- Εδώ έχουμε το VD View που δεν είναι developped και απλά δείχνει άκυρα δεδομένα -->
 <template>
-  <!--&lt;!&ndash;  Here we have the sidebar component that we need for navigation and features&ndash;&gt;-->
-  <div class="p-6 mx-2 min-w-full">
-    <div class="grid grid-cols-2 grid-rows-2 gap-x-8 gap-y-4 h-screen">
-      <CustomBarChart :labels="[]" dataKey="max_cell_voltage" borderColor="green" backgroundColor="rgba(0,255,0,0.3)"/>
-
+  <div class="p-35 mb-40 ml-10 mx-2 min-w-full">
+    <div class="grid grid-cols-2 grid-rows-3 gap-x-8 gap-y-4 h-screen">
+      <CustomLineChart
+          :labels="'ax'"
+          :items="ax"
+          :title="'ax'"
+      />
+      <CustomLineChart
+          :labels="'ay'"
+          :items="ay"
+          :title="'ay'"
+      />
+      <CustomGauge
+          :labels="'yaw_rate'"
+          :items="yaw_rate"
+          :title="'yaw_rate'"
+      />
     </div>
   </div>
-
 </template>
 
 <script lang="ts">
-import {defineComponent} from "vue"
-import CustomBarChart from "../components/CustomBarChart.vue"
-import SideBar from "../components/SideBar.vue";
+import { defineComponent, reactive } from 'vue';
+import SideBar from '../components/SideBar.vue';
+import BarChartComponent from '@/components/CustomBarChart.vue';
+
+import CustomLineChart from "@/components/CustomLineChart.vue";
+import CustomGauge from "@/components/GaugeChart.vue";
+import {io} from "socket.io-client";
+import {api_res, event_connection_res} from "../types/socketIO.types.ts"; //"@" instead of ".."
+import {linechartItems, VehicleTelemetry_data} from "../types/live_telemetry.ts";
+
+const socket = io(import.meta.env.VITE_SOCKET_URL).connect()
 
 
 export default defineComponent({
-  name: "VD_View",
+  name: 'CustomCharts',
   components: {
-    CustomBarChart,
-    SideBar
+    CustomLineChart,
+    CustomGauge
+  },
+  setup() {
+    const labels = reactive<string[]>([]);
+
+    return {
+      labels,
+    };
+  },
+  created() {
+    socket.on('telemetry_data', (telemetry_data : api_res) => {
+      //Here we are updating the object every time we get a new message from the socket,
+      console.log("Ok");
+      try {
+        this.telemetry_data_obj = JSON.parse(telemetry_data.data as unknown as string) as VehicleTelemetry_data;
+      } catch (e) {
+        return null;
+      }
+      if (this.telemetry_data_obj.max_cell_temp !== undefined) {
+        this.max_cell_temp.value = [this.telemetry_data_obj.max_cell_temp];
+      }
+    });
   },
   data() {
     return {
+      telemetry_data_obj: {} as VehicleTelemetry_data,
 
-    };
-  },
-  mounted() {
-
-  },
-  methods: {
-
+      ax: 0,
+      ay: 0,
+      yaw_rate: 0
+    }
   }
 })
+
 </script>
