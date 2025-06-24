0e6454882845ad5d4c38c9bb0f11d16be18a6d91
Giannis-Man-Prom <g.manousakis.promracing@gmail.com>
2025-06-24 17:46:29 +0300

    Add rasp file

---
+++ TelemetryApp/App/runrasp.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Create the virtual environment
+echo "Creating virtual environment..."
+python3 -m venv /home/user/App/venv
+
+# Activate the virtual environment
+echo "Activating virtual environment..."
+source home/user/App/venv/bin/activate
+
+# Ensure pip is installed and upgraded
+echo "Ensuring pip is installed..."
+python -m ensurepip --upgrade
+
+# Install dependencies
+echo "Installing dependencies
+pip install -r home/user/App/requirements.txt
+
+# Run the application
+echo "Running your application..."
+python home/user/App/app.py
+
+# Keep the terminal open after running
+read -p "Press enter to exit..."
